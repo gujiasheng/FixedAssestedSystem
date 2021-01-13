0ebbfba702684ece28d4ba51fb5162cf718bd7cd
gjs <909509848@qq.com>
2021-01-13 16:48:33 +0800

    登录页面完成

--- src/main/java/com/gjs/fixedassets/controller/LoginController.java
+++ src/main/java/com/gjs/fixedassets/controller/LoginController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class LoginController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "common/hello";
+
+    @GetMapping("tologin")
+    public String toLogin() {
+        return "login";
     }
 }
